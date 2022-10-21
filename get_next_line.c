35e2b5b6eb0f5496c11abd4b06d6b67023bc0f27
acerrah <cerrahalierdem@gmail.com>
2022-10-22 01:20:32 +0300

    getnextline finished

---
+++ get_next_line.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acerrah <acerrah@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 16:50:35 by acerrah           #+#    #+#             */
+/*   Updated: 2022/10/20 16:50:36 by acerrah          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = get_buffer(fd, buffer);
+	if (!buffer)
+		return (NULL);
+	line = get_line(buffer);
+	buffer = get_left(buffer);
+	return (line);
+}
+
+char	*get_left(char *left)
+{
+	char	*buff;
+	int		i;
+
+	i = 0;
+	while (left[i] && left[i] != '\n')
+		i++;
+	if (!left[i])
+	{
+		free(left);
+		return (NULL);
+	}
+	i++;
+	buff = ft_substr(left, i, (ft_strlen(left) - i));
+	if (!buff)
+		return (NULL);
+	free(left);
+	return (buff);
+}
+
+char	*get_line(char *buffer)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	if (!*(buffer + i))
+		return (NULL);
+	while (buffer[i] != '\n' && buffer[i] != '\0')
+		i++;
+	if (buffer[i] == '\n')
+		line = ft_substr(buffer, 0, i + 1);
+	else
+		line = ft_substr(buffer, 0, i);
+	if (!line)
+		return (NULL);
+	return (line);
+}
+
+char	*get_buffer(int fd, char *left_str)
+{
+	char	*buff;
+	int		rd_bytes;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	rd_bytes = 1;
+	while (!ft_strchr(left_str, '\n') && rd_bytes != 0)
+	{
+		rd_bytes = read(fd, buff, BUFFER_SIZE);
+		if (rd_bytes == -1)
+		{
+			free(buff);
+			free(left_str);
+			return (NULL);
+		}
+		buff[rd_bytes] = '\0';
+		left_str = ft_strjoin(left_str, buff);
+	}
+	free(buff);
+	return (left_str);
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acerrah <acerrah@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/20 16:32:31 by acerrah           #+#    #+#             */
+/*   Updated: 2022/10/20 16:32:33 by acerrah          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *s1);
+char	*get_left(char *left);
+char	*get_line(char *buffer);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char const *s2);
+char	*get_buffer(int fd, char *left_str);
+
+#endif
